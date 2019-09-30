2fc5a0c9640a6f2dda553fa772bcb5bd7e1ad3d7
leeiopd <leeiopd@hanmail.net>
2019-09-30 12:59:42 +0900

    190930

--- 190929/1_level3_프로그래머스_다리를지나는트럭.py
+++ 190929/1_level3_프로그래머스_다리를지나는트럭.py
@@ -1,24 +1,12 @@
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
     bridge = [0] * bridge_length
-    cnt = 0
-    while cnt < len(truck_weights):
+    answer = 0
+    while bridge:
         answer += 1
-        for i in range(bridge_length):
-            if bridge[i]:
-                if i == 0:
-                    bridge[i] = 0
-                else:
-                    bridge[i-1] = bridge[i]
-                    bridge[i] = 0
-        if sum(bridge)+truck_weights[cnt] <= weight:
-            truck = truck_weights[cnt]
-            bridge[-1] = truck
-            cnt += 1
-    answer += bridge_length
-
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
     return answer
-
-bridge_length = int(input())
-weight = int(input())
-truck_weights = list(map(int, input().split()))
