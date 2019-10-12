e52d7e7f8a7730a231db0a1bceb8c36b905f91b4
이주호 <leeiopd@hanmail.net>
2019-10-12 22:24:57 +0900

    191012

--- 191009/2_모의_5644_무선충전.py
+++ 191009/2_모의_5644_무선충전.py
@@ -1,8 +1,8 @@
 '''
 스마트폰을 무선 충전 할 때 최적의 BC (Battery Charger)를 선택하는 알고리즘을 개발하고자 한다. [그림 1]과 같이 가로 세로 10*10 영역의 지도가 주어졌을 때, 설치된 BC 정보는 다음과 같다.
- 
 
- 
+
+
 
 BC 1
 
@@ -34,35 +34,35 @@ BC 3
 
 70
 
- 
 
-               
+
+
                                                              [그림 1]
 
- 
 
- 
+
+
 
 BC의 충전 범위가 C일 때, BC와 거리가 C 이하이면 BC에 접속할 수 있다. 이때, 두 지점 A(XA, YA), B(XB, YB) 사이의 거리는 다음과 같이 구할 수 있다.
 
 D = |XA – XB| + |YA – YB|
 
 위의 [그림 1]에서 (4,3)과 (5,4) 지점은 BC 1과 BC 3의 충전 범위에 모두 속하기 때문에, 이 위치에서는 두 BC 중 하나를 선택하여 접속할 수 있다.
- 
 
-                       
+
+
                                                               [그림 2]
- 
+
 
 [그림 2]와 같이 사용자 A와 B의 이동 궤적이 주어졌다고 가정하자. T는 초(Second)를 의미한다. 예를 들어 5초에 사용자 A는 (5, 2) 지점에, 사용자 B는 (6, 9) 지점에 위치한다.
 
 매초마다 특정 BC의 충전 범위에 안에 들어오면 해당 BC에 접속이 가능하다. 따라서 T=5에 사용자 A는 BC 3에, 사용자 B는 BC 2에 접속할 수 있다. 이때, 접속한 BC의 성능(P)만큼 배터리를 충전 할 수 있다. 만약 한 BC에 두 명의 사용자가 접속한 경우, 접속한 사용자의 수만큼 충전 양을 균등하게 분배한다.
 
 BC의 정보와 사용자의 이동 궤적이 주어졌을 때, 모든 사용자가 충전한 양의 합의 최댓값을 구하는 프로그램을 작성하라.
- 
+
 
 [그림 2]에서 T=11일 때, 사용자 A는 BC 1과 3 둘 중 하나에 접속이 가능하다. 같은 시간에 사용자 B는 BC 1에 접속할 수 밖에 없다. 따라서 사용자 A가 같은 BC 1에 접속한다면 충전되는 양를 반씩 나눠 갖게 되어 비효율적이다. 따라서 사용자 A가 BC 3에 접속하는 것이 더 이득이다.
- 
+
 
 T=11
 
@@ -87,11 +87,11 @@ BC 1 (100)
 
 70 + 100 = 170
 
- 
- 
+
+
 
 위 예제에서 매 초마다 충전한 양은 다음과 같다. 따라서 총 충전한 양의 총합은 720 + 480 = 1200 이다.
- 
+
 
 시간(T)
 
@@ -231,7 +231,7 @@ Sum
 
 480
 
- 
+
 
 [제약사항]
 
@@ -243,7 +243,7 @@ Sum
 6. BC의 성능 P는 10이상 500이하의 짝수이다. (10 ≤ P ≤ 500)
 7. 사용자의 초기 위치(0초)부터 충전을 할 수 있다.
 8. 같은 위치에 2개 이상의 BC가 설치된 경우는 없다. 그러나 사용자A, B가 동시에 같은 위치로 이동할 수는 있다. 사용자가 지도 밖으로 이동하는 경우는 없다.
- 
+
 
 [입력]
 
@@ -251,7 +251,7 @@ Sum
 테스트 케이스의 첫 번째 줄에는 총 이동 시간(M), BC의 개수(A)가 주어진다.
 그 다음 2개의 줄에는 각각 사용자 A와 B의 이동 정보가 주어진다.
 한 사용자의 이동 정보는 M개의 숫자로 구성되며, 각각의 숫자는 다음과 같이 매초마다 이동 방향을 의미한다.
- 
+
 
 숫자
 
@@ -281,7 +281,7 @@ Sum
 그 다음 줄에는 A개의 줄에 걸쳐 BC의 정보가 주어진다.
 하나의 BC 정보는 좌표(X, Y), 충전 범위(C), 처리량(P)로 구성된다.
 
- 
+
 
 [출력]
 
@@ -296,11 +296,28 @@ T = int(input())
 dx = [0, 0, 1, 0, -1]
 dy = [0, -1, 0, 1, 0]
 
+
+def Next(x, y, d):
+    X = x + dx[d]
+    Y = y + dy[d]
+
+    return X, Y
+
+
+def makeBC(X, Y, P, C):
+    bcRange = []
+    for y in range(1, 11):
+        for x in range(1, 11):
+            if abs(X-x) + abs(Y-y) <= C:
+                bcRange.append([x, y])
+    return bcRange
+
+
 for case in range(1, T+1):
     M, A = map(int, input().split())
 
-    moveInfoA = list(map(int, input().split()))
-    moveInfoB = list(map(int, input().split()))
+    moveInfoA = [0] + list(map(int, input().split()))
+    moveInfoB = [0] + list(map(int, input().split()))
 
     maps = [[0 for x in range(11)] for y in range(11)]
 
@@ -309,16 +326,44 @@ for case in range(1, T+1):
     for a in range(A):
         # c : 충전범위, p: 성능
         x, y, c, p = map(int, input().split())
-        maps[y][x] = 1
-        BC[(x, y)] = [c, p]
-
+        maps[y][x] = p
+        BC[p, x, y] = makeBC(x, y, p, c)
     Ax = 1
     Ay = 1
     Bx = 10
     By = 10
+    result = 0
+    for i in range(M+1):
+        Atemp = []
+        Ax, Ay = Next(Ax, Ay, moveInfoA[i])
+        for key, value in BC.items():
+            if [Ax, Ay] in value:
+                Atemp.append(key)
 
-    for i in range(M):
-        Ax += dx[moveInfoA[i]]
-        Ay += dy[moveInfoA[i]]
-        Bx += dx[moveInfoB[i]]
-        By += dx[moveInfoB[i]]
+        Btemp = []
+        Bx, By = Next(Bx, By, moveInfoB[i])
+        for key, value in BC.items():
+            if [Bx, By] in value:
+                Btemp.append(key)
+
+        if Atemp and Btemp:
+            temp = 0
+            Atemp.sort(reverse=True)
+            Btemp.sort(reverse=True)
+            for a in Atemp:
+                for b in Btemp:
+                    if a != b:
+                        if a[0] + b[0] > temp:
+                            temp = a[0] + b[0]
+            if not temp:
+                temp = Atemp[0][0]
+            result += temp
+        else:
+            if Atemp:
+                Atemp.sort(reverse=True)
+                result += Atemp[0][0]
+            elif Btemp:
+                Btemp.sort(reverse=True)
+                result += Btemp[0][0]
+
+    print('#{} {}'.format(case, result))
