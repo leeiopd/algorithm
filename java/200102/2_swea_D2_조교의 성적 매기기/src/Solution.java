694b1d514874f07206fc17805a8a747aba0b035c
이주호 <leeiopd@hanmail.net>
2020-01-06 16:15:13 +0900

    200106|java

--- java/200102/2_swea_D2_조교의 성적 매기기/src/Solution.java
+++ java/200102/2_swea_D2_조교의 성적 매기기/src/Solution.java
@@ -4,12 +4,52 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		
 		int T = sc.nextInt();
-		int N, K;
-		for (int testCase = 1; testCase <= 10; testCase++) {
+		
+//		중간 35, 기말 45, 과제20
+		String[] grade = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"}; 
+		
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int N, K, A, B, C;
+			double checkScore;
+			int gradeNumPosition, gradeLimit;
 			N = sc.nextInt();
 			K = sc.nextInt();
+			double[] scores = new double[N];
+			gradeNumPosition = 0;
 			
+			checkScore = 0;
+			
+			for (int i = 0; i < N; i++) {
+				A = sc.nextInt();
+				B = sc.nextInt();
+				C = sc.nextInt();
+				
+				scores[i] = (A*0.35) + (B*0.45) + (C*0.20);
+				
+				if (i + 1 == K) {
+					checkScore = scores[i];
+				}
+			}
+			
+			Arrays.sort(scores);;
+			
+			gradeLimit = 0;
+			for (int i = N-1; i >= 0; i--) {
+				gradeLimit ++;
+				
+				if (scores[i] == checkScore) {
+					System.out.println("#"+testCase+" "+grade[gradeNumPosition]);
+					break;
+				}
+				
+				
+				if (gradeLimit == N/10) {
+					gradeLimit = 0;
+					gradeNumPosition ++;
+				}
+			}
 			
 		}
+		sc.close();
 	}
 }
