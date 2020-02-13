acc9ac8f33485c1b69e04981819a712daea8d0c9
leeiopd <leeiopd@gmail.com>
2020-02-13 10:14:04 +0900

    200213|java

--- java/200212/1_swea_D3_8500/src/Solution.java
+++ java/200212/1_swea_D3_8500/src/Solution.java
@@ -1,55 +1,43 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int T = Integer.parseInt(bf.readLine());
 		
-		for (int tc = 1; tc <= 10; tc++) {
+		for (int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(bf.readLine());
 			
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			
-			int[] nums = new int[N];
+			ArrayList<Integer> nums = new ArrayList<Integer>();
 			
 			for (int i = 0; i < N; i++) {
-				nums[i] = Integer.parseInt(st.nextToken());
+				nums.add(Integer.parseInt(st.nextToken()));
 			}
 			
-			sol(nums, N);
+			Collections.sort(nums);
 			
+			int result = 0;
+			for (int i = 0; i<N; i++) {
+				result += nums.get(i);
+			}
+			
+			result += nums.get(N-1) + N;
+			
+			bw.write("#"+tc+" "+result+"\n");
 		}
-	}
-	
-	public static int sol(int[] input, int N) {
-		int result = 0;
-		for (int i = 0; i < N; i++) {
-			num = Integer.parseInt(st.nextToken());
-			
-			if (i == 0) {
-				result += num;
-				temp = num;
-				continue;
-			}
-			
-			if (temp < num) {
-				result += num;
-				temp = num;
-			}
-			else {
-				result += temp;
-				temp = num;
-			}
-		}
-		result += num+N;
 		
-		
-		System.out.println(result);
-		
-	
+		bw.flush();
+		bw.close();
 	}
 }
