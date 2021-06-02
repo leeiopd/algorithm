2f8a4153064468c2b3792c4e425829a75891731b
Juho Lee <leeiopd@gmail.com>
2021-06-02 16:59:41 +0900

    210602
    boj 11653
    boj 2089

---
+++ python/210602_boj_2089.py
@@ -0,0 +1,14 @@
+N = int(input())
+result = ""
+
+while N != 0:
+    div = N // -2
+    if N % -2 < 0:
+        div += 1
+    result += str(N - (div*-2))
+    N = div
+
+if result:
+    print(result[::-1])
+else:
+    print(0)
