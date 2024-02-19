01b09ec9c904e37fb2c7bfba1055c9ae74b74cde
migrationArc <45933633+leeiopd@users.noreply.github.com>
2024-02-19 22:32:33 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 128 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const inputs = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map((n)=>parseInt(n));
+
+const N = inputs.shift()
+
+const list = new Array(12).fill(0)
+
+list[1] = 1
+list[2] = 2
+list[3] = 4
+
+for(let i = 4; i < 12; i++){
+    list[i] = list[i-1] + list[i-2] + list[i-3]
+}
+
+for(input of inputs){
+    console.log(list[input])
+}
+
